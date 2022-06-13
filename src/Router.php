5c5ea25fe62e8f6e446f1ba0dd88d92306f7b510
JonasAntonio <jayv.jonas@gmail.com>
2022-06-13 11:02:56 -0300

    router class

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,86 @@
 
 namespace Basic\Router;
 
+use Closure;
+
+/**
+ * Router singleton that adds routes to application
+ */
 class Router
 {
+    private static ?self $instance;
+    private Request $request;
+    private Routes $routes;
+
+    private function __construct()
+    {
+        $this->request = Request::instance();
+        $this->routes = new Routes();
+    }
+
+    public static function instance(): self
+    {
+        self::$instance ??= new Router();
+        return self::$instance;
+    }
+
+    /**
+     * Adds GET route
+     *
+     * @param string $endpoint
+     * @param Closure $executable
+     * @return Route
+     */
+    public function get(string $endpoint, Closure $executable): Route
+    {
+        return $this->routes->add(new Route($executable), "GET", $endpoint);
+    }
+
+    /**
+     * Adds POST route
+     *
+     * @param string $endpoint
+     * @param Closure $executable
+     * @return Route
+     */
+    public function post(string $endpoint, Closure $executable): Route
+    {
+        return $this->routes->add(new Route($executable), "POST", $endpoint);
+    }
+
+    /**
+     * Adds PUT route
+     *
+     * @param string $endpoint
+     * @param Closure $executable
+     * @return Route
+     */
+    public function put(string $endpoint, Closure $executable): Route
+    {
+        return $this->routes->add(new Route($executable), "PUT", $endpoint);
+    }
+
+    /**
+     * Adds DELETE route
+     *
+     * @param string $endpoint
+     * @param Closure $executable
+     * @return Route
+     */
+    public function delete(string $endpoint, Closure $executable): Route
+    {
+        return $this->routes->add(new Route($executable), "DELETE", $endpoint);
+    }
+
+    /**
+     * Adds PATCH route
+     *
+     * @param string $endpoint
+     * @param Closure $executable
+     * @return Route
+     */
+    public function patch(string $endpoint, Closure $executable): Route
+    {
+        return $this->routes->add(new Route($executable), "PATCH", $endpoint);
+    }
 }
