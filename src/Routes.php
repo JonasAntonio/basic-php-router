b76630513a81feae05b563ef2e5f2fc0970f140d
JonasAntonio <jayv.jonas@gmail.com>
2022-06-13 11:02:36 -0300

    feat: routes class

--- src/Routes.php
+++ src/Routes.php
@@ -4,4 +4,26 @@ namespace Basic\Router;
 
 class Routes
 {
+    /**
+     * Routes in application
+     *
+     * @var array<Route>
+     */
+    private array $routes;
+
+    public function add(Route $route, string $method, string $endpoint): Route
+    {
+        $this->routes[$method][$endpoint][] = $route;
+        return $route;
+    }
+
+    public function getAll()
+    {
+        return $this->routes;
+    }
+
+    public function get(string $method, string $endpoint): ?Route
+    {
+        return $this->routes[$method][$endpoint] ?? null;
+    }
 }
